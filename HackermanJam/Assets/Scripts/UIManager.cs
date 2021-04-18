f1d392cf05436096875b7f369cae4c83a8793572
ArmandoDLaRosa <37047175+ArmandoDLaRosa@users.noreply.github.com>
2021-04-18 12:53:43 -0500

    wfer
    efe

---
+++ HackermanJam/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text score;
+    public static int currentScore;
+    public static GameObject[] objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = 100;
+        score.text = "Puntos: "+currentScore;
+        //objects = GameObject.FindGameObjectsWithTag("PickUp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        score.text = "Vida: "+currentScore;
+    }
+}
---
+++ HackermanJam/Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateScore : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Piedra"){
+            UIManager.currentScore -= 1;
+            
+        }else {
+            if(collision.gameObject.tag == "Pez"){
+                UIManager.currentScore -= 5;
+            }
+        }
+    }
+}
